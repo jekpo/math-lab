e58963cd40a0e32cf60d60ecf6c5e876fd60df41
Israel Ekpo <israel@aicer.org>
2014-08-01 20:23:40 -0400

    Installing Inital set of entities

---
+++ src/test/java/com/ekpo/june/math/lab/test/SampleTest.java
@@ -0,0 +1,16 @@
+package com.ekpo.june.math.lab.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void checkOut() {
+
+        int expected = 5;
+        int actual = 5;
+
+        Assert.assertEquals(expected, actual);
+    }
+}
